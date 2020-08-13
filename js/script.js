1a27eb425b718b572f46ebc249b8665cd8a07d5b
Hadi <hadyfarhat97@gmail.com>
2020-08-13 18:40:46 +0100

    t-shirt info | hide, show and configure color options

--- js/script.js
+++ js/script.js
@@ -39,7 +39,64 @@ function setFocusOnOtherTitleField() {
 }
 
 
+/**
+ * Hide, display and configure the options of the 'color' select field based on
+ * the values of the 'design' select field
+ */
+function tShirtDesignFunctionality() {
+    let tShirtDesignField = document.querySelector('select#design');
+    tShirtDesignField.addEventListener('change', e => {
+        if (e.target.value == 'select') {
+            hideShirtColorsField();
+        } else {
+            displayShirtColorsField();
+            if (e.target.value == 'js puns') {
+                toggleSelectField('color', ['cornflowerblue', 'darkslategrey', 'gold']);
+            } else if (e.target.value == 'heart js') {
+                toggleSelectField('color', ['tomato', 'steelblue', 'dimgrey']);
+            }
+        }
+    });
+}
+
+
+/**
+ * Display only the passed options of the stated select field. Hide the others
+ */
+function toggleSelectField(id, options) {
+    let selectField = document.querySelector(`#${id}`);
+    if (selectField) {
+        for (let i = 0; i < selectField.options.length; ++i) {
+            let option = selectField.options[i];
+            if (options.includes(option.value)) {
+                option.classList.remove('is-hidden');
+            } else {
+                option.classList.add('is-hidden');
+            }
+        }
+    }
+}
+
+
+/**
+ * Remove hidden class from the 'shirt-colors' field
+ */
+function displayShirtColorsField() {
+    document.querySelector('div#shirt-colors').classList.remove('is-hidden');
+}
+
+
+/**
+ * Add hidden class to the 'shirt-colors' field
+ */
+function hideShirtColorsField() {
+    document.querySelector('div#shirt-colors').classList.add('is-hidden');
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
     hideOtherTitleField();
+    // hideShirtColorsField();
     jobRoleOtherTitleFunctionality();
+    tShirtDesignFunctionality();
 });
