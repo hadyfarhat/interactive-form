d80b1217cd016c7e137b4735241f61bb5b578bb9
Hadi <hadyfarhat97@gmail.com>
2020-08-14 14:36:30 +0100

    time formatting functionality
    - change 12 hour format to 24 hour format
    - get time slots between range

--- js/script.js
+++ js/script.js
@@ -30,6 +30,31 @@ function setFocusOnElementBySelector(selector) {
 
 
 /**
+ * Display only the passed options of the stated select element. Hide the others
+ * @param {string} selector - attribute of the select element
+ * @param {string[]} options - values of the options that shouldn't be hidden
+ */
+function toggleSelectElement(selector, options) {
+    let selectElement = document.querySelector(selector);
+    if (selectElement) {
+        let selectedIndices = [];
+        for (let i = 0; i < selectElement.options.length; ++i) {
+            let option = selectElement.options[i];
+            if (options.includes(option.value)) {
+                option.classList.remove('is-hidden');
+                selectedIndices.push(i);
+            } else {
+                option.classList.add('is-hidden');
+            }
+        }
+
+        // Mark the first non-hidden option as selected
+        selectElement.selectedIndex = selectedIndices[0];
+    }
+}
+
+
+/**
  * Display 'other title' input element when 'other' option
  * is selected from the 'job role' select element. Hide it when it's not selected.
  */
@@ -68,33 +93,80 @@ function tShirtDesignFunctionality() {
 
 
 /**
- * Display only the passed options of the stated select element. Hide the others
- * @param {string} selector - attribute of the select element
- * @param {string[]} options - values of the options that shouldn't be hidden
+ * Change 12 hour format to 24 hour format. ex 1pm changes to 13
+ * @example
+ * // returns 13
+ * change12to24HourFormat('1pm')
+ * @example
+ * // returns 9
+ * change12to24HourFormat('9am')
+ * @param {string} time - 12 hour format time
+ * @return {int} 24 hour format time
  */
-function toggleSelectElement(selector, options) {
-    let selectElement = document.querySelector(selector);
-    if (selectElement) {
-        let selectedIndices = [];
-        for (let i = 0; i < selectElement.options.length; ++i) {
-            let option = selectElement.options[i];
-            if (options.includes(option.value)) {
-                option.classList.remove('is-hidden');
-                selectedIndices.push(i);
-            } else {
-                option.classList.add('is-hidden');
-            }
-        }
-
-        // Mark the first non-hidden option as selected
-        selectElement.selectedIndex = selectedIndices[0];
+function change12to24HourFormat(time) {
+    let hour = parseInt(time.match(/^(\d+)/g)[0]);
+    let ampm = time.match(/\w\w$/g)[0];
+    
+    if (ampm == 'am') {
+        return (hour == 12) ? 0 : hour;
     }
+
+    return (hour == 12) ? 12 : hour + 12;
+}
+
+
+/**
+ * Accepts two 24 hour format time parameters and returns their 1 hour
+ * time slots range. 
+ * @example
+ * // returns ['11-12', '12-13', '13-14', '14-15']
+ * getTimeSlotsBetweenRange(11, 15)
+ * @param {int} start - start of time range
+ * @param {int} end - end of time range
+ * @return {string[]} time slots range
+ */
+function getTimeSlotsBetweenRange(start, end) {
+    let timeSlots = [];
+    for (let i = start; i < end; ++i) {
+        timeSlots.push(`${i}-${i+1}`);
+    }
+
+    return timeSlots;
+}
+
+
+/**
+ * Extracts the day from day and time format
+ * @example
+ * // returns 'Tuesday'
+ * getDayFromDayAndTimeStr('Tuesday 9am-12pm')
+ * @param {string} dayAndTime - day and time string format
+ * @return {string} returns the day from the date and time string
+ */
+function getDayFromDayAndTimeStr(dayAndTime) {
+    return dayAndTime.match(/^\w+/)[0];
+}
+
+
+/**
+ * Extracts the time from day and time string format
+ * @example 
+ * // returns ['9am', '12pm']
+ * getTimeFromDayAndTimeStr('Tuesday 9am-12pm')
+ * @param {string} dayAndTime - day and time string format
+ * @return {string[]} returns an array of the two 12 hour format times
+ */
+function getTimeFromDayAndTimeStr(dayAndTime) {
+    let timeSlot = /(\d+\w\w)-(\d+\w\w)$/.exec(dayAndTime);
+    return (timeSlot) ? [timeSlot[1], timeSlot[2]] : [];
 }
 
 
 document.addEventListener('DOMContentLoaded', () => {
     hideElementBySelector(otherTitleElementSelector);
     hideElementBySelector(shirtColorElementSelector);
+
     jobRoleOtherTitleFunctionality();
     tShirtDesignFunctionality();
+    activityRegistrationFunctionality();
 });
